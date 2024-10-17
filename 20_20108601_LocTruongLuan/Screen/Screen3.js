50ddc9f93fadcd1044458f697264a4bed1d518b3
luan2410 <loctruongluan12a5hd@gmail.com>
2024-10-18 03:19:44 +0700

    cau3 hoàn thiện chức năng đăng nhập

--- 20_20108601_LocTruongLuan/Screen/Screen3.js
+++ 20_20108601_LocTruongLuan/Screen/Screen3.js
@@ -1,41 +1,52 @@
 import { StyleSheet, Text, View, Image, TextInput } from 'react-native'
 import React from 'react'
 import { TouchableOpacity } from 'react-native'
-import Screen4 from './Screen4'
 import { color } from 'react-native-elements/dist/helpers'
-
-export default function Screen3({navigation}) {
-
+import { useState } from 'react'
+export default function Screen3({navigation,route}) {
+    const { userInfo } = route.params; // This gets the user info passed from Screen2
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleLogin = () => {
+      // Check if the username and email match with the stored userInfo
+      if (email === userInfo.Uemail && password === userInfo.Upassword) {
+        // Navigate to Screen_04 if credentials match
+        navigation.navigate('Screen4');
+      } else {
+        alert('Invalid login details!');
+      }
+    }
   return (
     <View style={styles.container}>
         <View style={styles.view1} >
                 <Image source={require('../assets/DATA/Image20.png')} style={{width:'100%',}} />
         </View>
         <View style={styles.view2} >
-            <Text style={{fontSize:30,fontWeight: 'bold',color:'black'}}>
+            <Text style={{fontSize:30,fontWeight: 'bold',color:'black', alignSelf: 'flex-start',marginLeft:20}}>
                 WellCome!
             </Text>
-            <Text style={{marginTop: 50,fontWeight: 'bold',}}>
+            <Text style={{marginTop: 50,fontWeight: 'bold', alignSelf: 'flex-start',marginLeft:20}}>
                 Email
             </Text>
             <View style={styles.viewip}>
                 <Image source={require('../assets/DATA/Vector.png')} style={styles.img} />
-                <TextInput placeholder='Enter email' style={styles.ip}/>
+                <TextInput placeholder='Enter email' style={styles.ip} value={email} onChangeText={setEmail} />
             </View>
-            <Text style={{fontWeight: 'bold',}}>
+            <Text style={{fontWeight: 'bold', alignSelf: 'flex-start', marginLeft:20 ,}}>
                 password
             </Text>
             <View style={styles.viewip}>
                 <Image style={styles.img} source={require('../assets/DATA/lock.png')} />
-                <TextInput style={styles.ip} placeholder='enter password' secureTextEntry />
+                <TextInput style={styles.ip} placeholder='enter password' secureTextEntry value={password} onChangeText={setPassword} />
                 <Image style={styles.img} source={require('../assets/DATA/eye.png')} />
             </View>
         </View>
         <View style={styles.view3}>
-            <TouchableOpacity onPress={()=> navigation.navigate('Screen4') }
+            <TouchableOpacity onPress={handleLogin}
             style={{width:'90%', height:50,textAlign: 'center', alignItems: 'center', borderRadius: 20, marginTop:20, backgroundColor: 'aqua', justifyContent: 'center',marginTop: 100}}
              >
-                    <Text style={{color: 'white'}}>
+                    <Text style={{color: 'white' , fontWeight: 'bold',}}>
                         Login
                     </Text>
             </TouchableOpacity>
@@ -55,23 +66,25 @@ const styles = StyleSheet.create({
     view2:{
         flex:3,
         flexDirection: 'column',
-        marginLeft:10
+        marginLeft:10,
+        alignItems: 'center',   
 
     }
     , view3:{
         flex:3,
         marginTop: "10%",
-        marginLeft: 10
+        marginLeft: 10, 
+        alignItems: 'center',
     },
     viewip:{
-        width:'95%',
+        width:'90%',
         height: 50,
-        backgroundColor:'gray',
+        backgroundColor:'white',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
         marginBottom: 10,
-        borderRadius: 15
+        borderRadius: 15,
     },
     ip:{
         width: '85%',
@@ -79,7 +92,7 @@ const styles = StyleSheet.create({
         marginRight: 5,
     },
     img:{
-        width: '7%',
+        width: '5%',
         height: '40%',
     }
 })
